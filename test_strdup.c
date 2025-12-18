c969b519834cccb137bf96f06c8b3fef7e41ebcb
eandres <eandres@student.42urduliz.com>
2025-12-18 19:07:27 +0100

    update test

--- test_strdup.c
+++ test_strdup.c
@@ -2,13 +2,22 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include "include/ft_malloc.h"
 
 int main(void)
 {
 	char *str;
 
 	str = malloc(15);
+
+	unsigned char c = (unsigned char)str[0];
+	printf("Valor en memoria :0x%X\n", c);
+
 	strcpy(str, "Hola");
+
+	unsigned char s = (unsigned char)str[0];
+	printf("Valor en memoria 2 :0x%X\n", s);
+
 	printf("String inicial: %s (Direccion: %p)\n", str, str);
 
 	str = realloc(str, 20);
@@ -22,6 +31,14 @@ int main(void)
 	str = realloc(str, 20);
 	printf("String reducido: %s (Direccion: %p)\n", str, str);
 
+	show_alloc_mem();
+
+	unsigned char q = (unsigned char)str[0];
+	printf("Valor en memoria 2 :0x%X\n", q);
+
+	printf("\nBONUS INFO\n\n");
+
+	show_alloc_mem_ex();
 	free(str);
 
 	return (0);
